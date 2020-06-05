59ed4f75b30a71decbd3a53e8c0f2d5b7ab7bfe6
James Grieve <pocojames@hotmail.com>
2020-06-04 22:35:38 -0700

    add adjacent_method

--- word_chainer.rb
+++ word_chainer.rb
@@ -1,3 +1,15 @@
+require "set"
 class WordChainer
+    attr_accessor :dictionary
+    def initialize(dictionary_file_name)
+        @dictionary = File.readlines(dictionary_file_name).map(&:chomp).to_set
+        #Faster lookup in dictionary .include?
+    end
 
+    def adjacent_words(word)
+
+    end
 end
+
+g = WordChainer.new("dictionary.txt")
+p g.dictionary
