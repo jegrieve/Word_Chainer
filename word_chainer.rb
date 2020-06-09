1b39823cd8689702e286c13c1817d766e6123846
James Grieve <pocojames@hotmail.com>
2020-06-08 23:33:52 -0700

    run method can take possible words

--- word_chainer.rb
+++ word_chainer.rb
@@ -1,10 +1,8 @@
 require "set"
 class WordChainer
-    attr_accessor :dictionary
-    def initialize(dictionary_file_name, source)
+    attr_reader :dictionary
+    def initialize(dictionary_file_name)
         @dictionary = File.readlines(dictionary_file_name).map(&:chomp).to_set
-        @current_words = [source]
-        @all_seen_words = [source]
     end
 
     def adjacent_words(word)
@@ -21,7 +19,7 @@ class WordChainer
                 adj_words << ele if count == 0
             end
         end
-        puts adj_words
+        adj_words
     end
 
     def same_length?(word, dictionary_word)
@@ -32,9 +30,45 @@ class WordChainer
     end
 
     def run(source, target)
+        indexes = {}
+        source.split("").each_with_index do |letter1, i1|
+            indexes[i1] = letter1 if letter1 == target[i1]
+        end
 
+        words = adjacent_words(source)
+        possible_words = []
+        current_word = []
+        words.each do |word,i1|
+            indexes.each do |k,v|
+                if word[k] != indexes[k]
+                else
+                    possible_words << word
+                end
+            end
+        end
+        word_chain = []
+        while 
+    #     new_possible_words = []
+    #    possible_words.each do |word|
+    #         word.each_char.with_index do |letter, i|
+    #             if letter == target[i] && indexes[i].nil?
+    #                 new_possible_words << word
+    #             end
+    #         end
+    #     end
+            # while possible_words.length != 1
+        #     attempted_words = []
+        #     current_word = []
+        # end
+        
+        p possible_words
     end
+    #2. Go through adj words and first check if the word contains all the letter(s) in the hash
+    #3. If this word has an additional correct letter, add that letter to hash
+    #3. Keep going until source == target or if the possible words is empty we start over from  next word.
+    
 end
 
 g = WordChainer.new("dictionary.txt")
-g.adjacent_words("cat")
+
+g.run("math", "barn")
